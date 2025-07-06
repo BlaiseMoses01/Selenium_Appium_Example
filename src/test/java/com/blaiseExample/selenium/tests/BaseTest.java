83f547a2801ab95a3cebc1f170606d7d393d47ea
BlaiseMoses01 <97707627+BlaiseMoses01@users.noreply.github.com>
2025-07-06 13:01:05 -0400

    adding chrome options and headless run support

--- src/test/java/com/blaiseExample/selenium/tests/BaseTest.java
+++ src/test/java/com/blaiseExample/selenium/tests/BaseTest.java
@@ -1,8 +1,8 @@
 package com.blaiseExample.selenium.tests;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,20 +10,35 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
     
-    //inheritable webdriver browser instance
+    // inheritable webdriver browser instance
     protected WebDriver driver;
 
     // initialize the browser instance before each test 
     @BeforeMethod
-    public void setupTests(){
-        // using WebDriverManager to automatically get ChromeDriver for machine's current version
+    public void setupTests() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(); 
+
+        // Create ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+
+        // Check the "headless" system property
+        String headlessProp = System.getProperty("headless", "false");
+        boolean isHeadless = headlessProp.equalsIgnoreCase("true");
+
+        if (isHeadless) 
+            options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-user-data-" + System.currentTimeMillis());
+
+        driver = new ChromeDriver(options);
     }
 
     // close browser instance after each test 
     @AfterMethod
-    public void teardownTests(){
-        driver.quit(); 
+    public void teardownTests() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
